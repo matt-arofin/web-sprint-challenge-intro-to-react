863cb5ca660553843a7a78d452d3fe5de688b19e
Matthew Arofin <Matt.arofin@gmail.com>
2022-05-21 13:56:22 -0400

    First  Character Component Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Character from './components/Character';
+import axios from 'axios';
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characters, setCharacters] = useState();
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res =>
+        {console.log(res);
+        setCharacters(res.data);
+      }).catch(err => console.error(err))
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -13,7 +24,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character />
+      <Character characters={characters} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,16 @@
 // Write your Character component here
 
- const Character = () => {
+const Character = (props) => {
+    console.log("Character component props: " , props.characters);
+    // props.characters.map(() => console.log(props.characters.name))
 
+    /* return (
+        props.characters.map(() => 
+            <article>
+                <h3>{props.characters.name}</h3>
+            </article>
+        )
+    ) */
 }
 
 export default  Character
